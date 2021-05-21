34f286f1801c19a58f67603074b955912d35c3ae
sharansriramoju <sharan.sriramoju07@gmail.com>
2021-05-21 20:35:39 +0530

    gridview change

--- tictactoe/lib/main.dart
+++ tictactoe/lib/main.dart
@@ -20,6 +20,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> symbol = ["", "", "", "", "", "", "", "", ""];
   bool _image = false;
   Widget myWidget() {
     if (_image == false) {
@@ -34,8 +35,8 @@ class _HomePageState extends State<HomePage> {
         height: 200,
         decoration: BoxDecoration(
           image: DecorationImage(
-              image: NetworkImage(
-                "https://static.techgrapple.com/wp-content/uploads/2016/08/red-sky-whatsapp-chat-theme.jpg",
+              image: AssetImage(
+                "assets/x.jpg",
               ),
               fit: BoxFit.cover),
         ),
@@ -45,22 +46,52 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: Text("tic tac toe"),
       ),
       body: Container(
+        height: height,
         color: Colors.black87,
         child: Center(
-          child: GestureDetector(
-            onTap: () {
-              setState(() {
-                _image = true;
-              });
-            },
-            child: myWidget(),
-          ),
+          child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 1,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                crossAxisCount: 3,
+              ),
+              itemCount: 9,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (_image == false) {
+                        symbol[index] = "x";
+                        _image = true;
+                      } else {
+                        symbol[index] = "o";
+                        _image = false;
+                      }
+                    });
+                  },
+                  child: Container(
+                    color: Colors.white,
+                    child: Center(
+                      child: Text(
+                        symbol[index],
+                        style: TextStyle(
+                          fontSize: 120,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }),
         ),
       ),
     );
